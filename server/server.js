8351757ef91e47286405c53debd14c1a26c4e3ce
Zan Marolt <marolt.zan@hotmail.com>
2016-01-30 11:18:25 +0100

    added some functionality

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
 
+var serveIndex = require('serve-index');
+var serveStatic = require('serve-static');
+
 var PORT = process.env.PORT || 3000;
 
 exports.start = function(){
 
-    app.get('/', function(req, res){
-
-        res.send('Hello World!');
-
-    });
+    app.use ('/', serveStatic('public'));
+    app.use ('/', serveIndex('public'));
 
     app.listen(PORT, function(){
 
